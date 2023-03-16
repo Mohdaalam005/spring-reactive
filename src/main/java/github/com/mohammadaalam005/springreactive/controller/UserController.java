66cbf0b756a6c2ab099a0da65f87526d287788b3
mohd aalam <mohdaalam005@gmail.com>
2023-03-16 18:53:09 +0530

    added new method

--- src/main/java/github/com/mohammadaalam005/springreactive/controller/UserController.java
+++ src/main/java/github/com/mohammadaalam005/springreactive/controller/UserController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -25,5 +27,10 @@ public class UserController {
     @PostMapping(value = "/users",produces = MediaType.APPLICATION_JSON_VALUE , consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Mono<UserResponse>>createUser(@RequestBody UserRequest userRequest){
         return new ResponseEntity<>(userService.createUser(userRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/users",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Flux<UserRequest>>getUsers(){
+        return new ResponseEntity<>(userService.getUsers(),HttpStatus.OK);
     }
 }
--- src/main/java/github/com/mohammadaalam005/springreactive/service/UserService.java
+++ src/main/java/github/com/mohammadaalam005/springreactive/service/UserService.java
@@ -7,6 +7,7 @@ import github.com.mohammadaalam005.springreactive.model.UserResponse;
 import github.com.mohammadaalam005.springreactive.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -24,7 +25,12 @@ public class UserService {
     public Mono<UserResponse> createUser(UserRequest userRequest) {
         UserEntity userEntity = userMapper.modelToEntity(userRequest);
         Mono<UserEntity> save = userRepository.save(userEntity);
-        return save.map(userEntity1 -> userMapper.entityToResponse(userEntity1));
+        return save.map(userMapper::entityToResponse);
+    }
+
+    public Flux<UserRequest> getUsers(){
+        Flux<UserEntity> users = userRepository.findAll();
+        return users.map(userMapper::entityToModel);
     }
 
 
